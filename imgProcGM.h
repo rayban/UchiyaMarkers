bf48c5c9f22598c2a5f03057f19db0846bd1c6eb
antoine <antoinenash@gmail.com>
2011-10-19 21:13:11 +0200

    code d'origine :q

---
+++ imgProcGM.h
@@ -0,0 +1,18 @@
+//
+//  imgProcGM.h
+//  RandomDotMarkers
+//
+//  Created by Guillaume Moreau on 19/05/11.
+//  Copyright 2011 Ecole Centrale de Nantes. All rights reserved.
+//
+
+
+#include "Skeletonize.h"
+
+class imgProcGM : public Skeletonize {
+public:
+   // constructor
+   imgProcGM(IplImage *src);
+   
+   void processImage(IplImage *colorSrcImg, int threshold=230);
+};
---
+++ skeletonization/Skeletonize.h
@@ -0,0 +1,29 @@
+#ifndef SJ_SKELETONIZE_H_22022011142139
+#define SJ_SKELETONIZE_H_22022011142139
+
+#include <list>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+class Skeletonize
+{
+public:
+   Skeletonize(IplImage *src);
+   void operator()(void);
+   void skeletonize();
+   
+   void updateImage(IplImage *img);
+   
+   virtual void processImage(IplImage *colorSrcImg, int threshold=230);
+
+   
+protected:
+   IplImage *m_src;
+   static const int s_lut[256];
+   
+   int thin(unsigned int pass);
+};
+
+
+#endif
